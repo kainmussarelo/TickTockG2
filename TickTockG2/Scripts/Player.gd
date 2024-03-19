726ac6be6d6d9d8c3876d1167529cf0d368bbb98
Kain <92188358+kainmussarelo@users.noreply.github.com>
2024-03-19 23:28:14 +0000

    Game Src Code
    i dunno if it's working XD

---
+++ TickTockG2/Scripts/Player.gd
@@ -0,0 +1,13 @@
+extends KinematicBody
+
+export var speed = 100
+export var fov = 90
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
